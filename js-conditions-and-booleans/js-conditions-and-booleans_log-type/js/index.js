2047be5a6fe9ce183448b0914e462c4fd42aae5e
Angela Capillo <hello@angelcapillo.com>
2025-03-10 13:14:16 +0100

    Add new solution to consider all data types

--- js-conditions-and-booleans/js-conditions-and-booleans_log-type/js/index.js
+++ js-conditions-and-booleans/js-conditions-and-booleans_log-type/js/index.js
@@ -1,20 +1,49 @@
 let data = NaN;
 console.log(typeof data)
 
-switch (typeof data) {
-    case "string":
-        console.log("string!")
+// switch (typeof data) {
+//     case "string":
+//         console.log("string!")
+//         break;
+//     case "number":
+//         console.log("number!")
+//         break;
+//     case "boolean":
+//         console.log("boolean!")
+//         break;
+//     case "undefined":
+//         console.log("undefined!")
+//         break;
+//     default:
+//         console.log("I have no idea!")
+//         break;
+// }
+
+switch (true) {
+    case typeof data === "string":
+        console.log("string!");
         break;
-    case "number":
-        console.log("number!")
+    case typeof data === "number":
+        console.log(isNaN(data) ? "not a number!" : "number!");
         break;
-    case "boolean":
-        console.log("boolean!")
+    case typeof data === "boolean":
+        console.log("boolean!");
         break;
-    case "undefined":
-        console.log("undefined!")
+    case data === undefined:
+        console.log("undefined!");
+        break;
+    case data === null:
+        console.log("null!");
+        break;
+    case typeof data === "function":
+        console.log("function!");
+        break;
+    case Array.isArray(data):
+        console.log("array!");
+        break;
+    case typeof data === "object":
+        console.log("object!");
         break;
     default:
-        console.log("I have no idea!")
-        break;
+        console.log("I have no idea!");
 }
