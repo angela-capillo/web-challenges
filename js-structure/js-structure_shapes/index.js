9d023b3ea85bfc46d21b9009a49468759e0baa0f
Angela Capillo <hello@angelcapillo.com>
2025-03-18 19:15:43 +0100

    Add solution

--- js-structure/js-structure_shapes/index.js
+++ js-structure/js-structure_shapes/index.js
@@ -1,23 +1,13 @@
+import Circle from "./components/Circle/Circle";
+import Pentagon from "./components/Pentagon/Pentagon";
+import Square from "./components/Square/Square";
+
 console.clear();
 
 const root = document.getElementById("root");
 
-const circle = document.createElement("div");
-circle.classList.add("circle");
-circle.addEventListener("click", () => {
-  circle.style.backgroundColor = "#ccc";
-});
+const circleElement = Circle();
+const pentagonElement = Pentagon();
+const squareElement = Square();
 
-const square = document.createElement("div");
-square.classList.add("square");
-square.addEventListener("click", () => {
-  square.style.backgroundColor = "#ccc";
-});
-
-const pentagon = document.createElement("div");
-pentagon.classList.add("pentagon");
-pentagon.addEventListener("click", () => {
-  pentagon.style.backgroundColor = "#ccc";
-});
-
-root.append(circle, square, pentagon);
+root.append(circleElement, squareElement, pentagonElement);
