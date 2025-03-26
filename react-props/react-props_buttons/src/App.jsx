8c231b5f05004fd53e6a4f320949d0d4b299795f
Angela Capillo <hello@angelcapillo.com>
2025-03-26 15:13:00 +0100

    Add solution for task 1

--- react-props/react-props_buttons/src/App.jsx
+++ react-props/react-props_buttons/src/App.jsx
@@ -1,3 +1,15 @@
+function Button({ text, color, isDisabled }) {
+  return (<button style={{backgroundColor: color}} disabled={isDisabled}>
+    {text}
+  </button>)
+}
+
 export default function App() {
-  return <h1>Replace me with your component!</h1>;
+  return (
+    <div>
+      <Button text="Cancel" color="salmon" />
+      <Button text="Edit" color="rebeccapurple" />
+      <Button text="Continue" color="lime" isDisabled />
+    </div>
+  );
 }
