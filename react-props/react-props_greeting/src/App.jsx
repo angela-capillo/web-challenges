5371ae3e1a8dc0f9f5e90457234373f8984e4968
Angela Capillo <hello@angelcapillo.com>
2025-03-26 11:48:59 +0100

    Edit solution with conditional rendering

--- react-props/react-props_greeting/src/App.jsx
+++ react-props/react-props_greeting/src/App.jsx
@@ -1,8 +1,8 @@
 function Greeting({ name }) {
-  
-  return <h1> Hello {name}!</h1>
+
+  return <h1> { name === "Marcell" ? "Hello coach!" : `Hello ${name} !`}</h1>
 }
 
 export default function App() {
-  return <Greeting name="Sunshine" />;
+  return <Greeting name="Marcell" />;
 }
