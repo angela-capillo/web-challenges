df900e8d19b2f98601cc2435136d39316bc9ee66
Angela Capillo <hello@angelcapillo.com>
2025-04-08 11:07:43 +0200

    Add folder structure for routes

---
+++ nextjs-basics-and-routing/lotr-app/pages/volumes/volumes.jsx
@@ -0,0 +1,10 @@
+import { introduction } from "../lib/data";
+
+export default function Volumes() {
+    return (
+        <>
+        <h1>Lord of the Rings</h1>
+        <p></p>
+        </>
+    );
+}
