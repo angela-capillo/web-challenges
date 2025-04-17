41b5273410628a28a631012e6f07f695681ccf3e
Angela Capillo <hello@angelcapillo.com>
2025-04-17 10:39:44 +0200

    Add exercise for component testing session

--- react-component-testing/react-component-testing_scorekeeper/components/Player/Player.test.js
+++ react-component-testing/react-component-testing_scorekeeper/components/Player/Player.test.js
@@ -2,6 +2,16 @@ import { render, screen } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 import Player from ".";
 
-test("renders player information and two buttons", () => {});
+test("renders player information and two buttons", () => {
+    render(<Player name="Angela" score="130" onDecreasePlayerScore="" onIncreasePlayerScore="" />)
+
+    const playerName = screen.getByText("Angela");
+    const playerScore = screen.getByText("130");
+
+    
+
+
+    
+});
 
 test("calls callbacks when increasing or decreasing score", async () => {});
